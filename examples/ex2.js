9456daff693bade068a6435b630700cf6efa46f2
Edmond Meinfelder <edmond@stdarg.com>
2013-09-20 15:36:17 -0700

    Renamed files.

--- examples/ex2.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-var Discovery = require('../index.js').Discovery;
-var discover = new Discovery();
-discover.on('available', function(name, available, data, reason) {
-  console.log(name,':','available: '+available,reason);
-  console.log(data);
-});
