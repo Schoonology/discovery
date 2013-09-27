7e03fa10631e014d28f249a0188f5aad203d0551
Michael Schoonmaker <michael.r.schoonmaker@gmail.com>
2013-09-26 21:38:36 -0700

    Add a couple more test scenarios.
    The test scenario with an update crashes Node and causes undefined
    behaviour, so we need to work that out first.

---
+++ test/fixtures/udp-upupdatedown.js
@@ -0,0 +1,17 @@
+var discovery = require('../../');
+var registry = discovery.createRegistry({ id: 'udp-upupdatedown' });
+var service = registry.createService('test', { foo: 'bar' });
+
+setTimeout(function () {
+  service.update({
+    bar: 'foo'
+  });
+}, 100);
+
+setTimeout(function () {
+  service.available = false;
+}, 200);
+
+setTimeout(function () {
+  process.exit(0);
+}, 300);
--- test/udp.js
+++ test/udp.js
@@ -4,6 +4,27 @@ var discovery = require('../');
 
 describe('UDP Broadcast', function () {
   var registry;
+  var events;
+
+  beforeEach(function () {
+    registry = discovery.createRegistry();
+
+    registry.on('available', function (name, service, reason) {
+      expect(service.available).to.equal(true);
+      events.push('available');
+    });
+
+    registry.on('unavailable', function (name, service, reason) {
+      expect(service.available).to.equal(false);
+      events.push('unavailable');
+    });
+
+    registry.on('update', function (name, service, reason) {
+      events.push('update');
+    });
+
+    events = [];
+  });
 
   afterEach(function () {
     if (registry) {
@@ -12,33 +33,95 @@ describe('UDP Broadcast', function () {
   });
 
   it('up-down', function (done) {
-    var events = [];
-
-    registry = discovery.createRegistry();
-
     registry.on('available', function (name, service, reason) {
       expect(name).to.equal('test:udp-updown');
       expect(service.name).to.equal('test:udp-updown');
-      expect(service.available).to.equal(true);
       expect(service.local).to.equal(false);
       expect(service.data.foo).to.equal('bar');
       expect(reason).to.equal('new');
-
-      events.push('available');
     });
 
     registry.on('unavailable', function (name, service, reason) {
       expect(name).to.equal('test:udp-updown');
       expect(service.name).to.equal('test:udp-updown');
-      expect(service.available).to.equal(false);
       expect(service.local).to.equal(false);
       expect(service.data.foo).to.equal('bar');
       expect(reason).to.equal('availabilityChange');
-
-      events.push('unavailable');
     });
 
     common.forkServiceTest('udp-updown')
+      .on('exit', function (code) {
+        expect(code).to.equal(0, 'Subprocess did not exit nicely.');
+
+        expect(events).to.deep.equal([
+          'available',
+          'unavailable'
+        ]);
+
+        done();
+      });
+  });
+
+  it('up-down-up', function (done) {
+    registry.on('available', function (name, service, reason) {
+      expect(name).to.equal('test:udp-updownup');
+      expect(service.name).to.equal('test:udp-updownup');
+      expect(service.local).to.equal(false);
+      expect(service.data.foo).to.equal('bar');
+      expect(reason).to.equal('new');
+    });
+
+    registry.on('unavailable', function (name, service, reason) {
+      expect(name).to.equal('test:udp-updownup');
+      expect(service.name).to.equal('test:udp-updownup');
+      expect(service.local).to.equal(false);
+      expect(service.data.foo).to.equal('bar');
+      expect(reason).to.equal('availabilityChange');
+    });
+
+    common.forkServiceTest('udp-updownup')
+      .on('exit', function (code) {
+        expect(code).to.equal(0, 'Subprocess did not exit nicely.');
+
+        expect(events).to.deep.equal([
+          'available',
+          'unavailable',
+          'available'
+        ]);
+
+        done();
+      });
+  });
+
+  it('up-update-down', function (done) {
+    registry.on('available', function (name, service, reason) {
+      expect(name).to.equal('test:udp-upupdatedown');
+      expect(service.name).to.equal('test:udp-upupdatedown');
+      expect(service.local).to.equal(false);
+      expect(service.data.foo).to.equal('bar');
+      expect(service.data.bar).to.not.exist;
+      expect(reason).to.equal('new');
+    });
+
+    registry.on('update', function (name, service, reason) {
+      expect(name).to.equal('test:udp-upupdatedown');
+      expect(service.name).to.equal('test:udp-upupdatedown');
+      expect(service.local).to.equal(false);
+      expect(service.data.foo).to.equal('bar');
+      expect(service.data.bar).to.equal('foo');
+      expect(reason).to.equal('update');
+    })
+
+    registry.on('unavailable', function (name, service, reason) {
+      expect(name).to.equal('test:udp-upupdatedown');
+      expect(service.name).to.equal('test:udp-upupdatedown');
+      expect(service.local).to.equal(false);
+      expect(service.data.foo).to.equal('bar');
+      expect(service.data.bar).to.equal('foo');
+      expect(reason).to.equal('availabilityChange');
+    });
+
+    common.forkServiceTest('udp-upupdatedown')
       .on('exit', function (code) {
         expect(code).to.equal(0, 'Subprocess did not exit nicely.');
 
